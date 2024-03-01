324225ef88a9f81323903f2611656825cd47c006
OSCAR SANTIAGO VELEZ MUÑOZ <90299049+ossavemu@users.noreply.github.com>
2024-03-01 18:56:30 -0500

    Añadir cambios en los componentes Card, Layout, theme/utils/icon, layout/Background, Peeps, icons/Sun, icons/Moon, NavModal y PublishSlider

--- src/lib/theme/utils/icon.ts
+++ src/lib/theme/utils/icon.ts
@@ -1,9 +1,8 @@
-const moonBtn = document.getElementById('moon')
-const sunBtn = document.getElementById('sun')
-
-const event = new Event('storage')
-
 export const setIcon = (isDark: boolean): void => {
+  const moonBtn = document.getElementById('moon')
+  const sunBtn = document.getElementById('sun')
+
+  const event = new Event('storage')
   if (moonBtn == null || sunBtn == null) return
   moonBtn.classList.toggle('hidden', isDark)
   sunBtn.classList.toggle('hidden', !isDark)
